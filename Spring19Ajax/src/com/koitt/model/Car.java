6ff703a707e276bc986e84fcd73de40ce1f020ed
KOITT <KOITT@KOITT-PC18>
2018-04-09 15:08:47 +0900

    BoardRest 설정 초기

---
+++ Spring19Ajax/src/com/koitt/model/Car.java
@@ -0,0 +1,75 @@
+package com.koitt.model;
+
+public class Car {
+	private String name;
+	private String number;
+	
+	public Car() {}
+
+	public Car(String name, String number) {
+		this.name = name;
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Car [name=");
+		builder.append(name);
+		builder.append(", number=");
+		builder.append(number);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
---
+++ Spring20BoardRest/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<!DOCTYPE html>
+<html>
+<head><title>홈페이지</title></head>
+<body>
+	<h1>홈페이지</h1>
+	<div>현재 언어 : ${ pageContext.response.locale }</div>
+	<a href="<c:url value='/board/board-list.do'/>"><spring:message code="list"/></a><br>
+	<a href="<c:url value='/admin/users-list.do'/>"><spring:message code="userlist"/></a><br>
+	<sec:authorize access="!hasRole('ADMIN') and !hasRole('USER')">
+		<a href="<c:url value='/join.do'/>"><spring:message code="signA"/></a><br>
+		<a href="<c:url value='/login.do'/>"><spring:message code="login"/></a><br>
+	</sec:authorize>
+	<sec:authorize access="hasRole('ADMIN') or hasRole('USER')">
+		<a href="<c:url value='/users-modify.do'/>"><spring:message code="edituserI"/></a>
+		<a href="<c:url value='/logout.do'/>"><spring:message code="logout"/></a><br>
+	</sec:authorize>
+	<a href="index.do?lang=ko"><spring:message code="label.ko"/></a>
+	<a href="index.do?lang=en"><spring:message code="label.en"/></a>
+</body>
+</html>
